6c794a70eeaa0107821d463ea430793563a49b69
bartoluzuzazo <bartbo62@gmail.com>
2023-06-17 18:59:30 +0200

    added stock searching

---
+++ Client/Models/DTOs/StockSearchDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Proj_APBD.Client.Models.DTOs;
+
+public class StockSearchDTO
+{
+    public string ticker { get; set; }
+    public string name { get; set; }
+    /*public string market{ get; set; }
+    public string locale{ get; set; }
+    public string primary_exchange{ get; set; }
+    public string type{ get; set; }
+    public string active{ get; set; }
+    public string currency_name{ get; set; }
+    public string cik{ get; set; }
+    public string composite_figi{ get; set; }
+    public string share_class_figi{ get; set; }
+    public string last_updated_utc { get; set; }*/
+}
---
+++ Client/Models/DTOs/StockSearchResultsDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Proj_APBD.Client.Models.DTOs;
+
+public class StockSearchResultsDTO
+{
+    public List<StockSearchDTO> results { get; set; }
+}
