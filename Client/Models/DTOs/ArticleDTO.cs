4d52a1629cbc2af637c236c01860862edb5e4b80
bartoluzuzazo <bartbo62@gmail.com>
2023-06-23 01:25:42 +0200

    improved article ui

--- Client/Models/DTOs/ArticleDTO.cs
+++ Client/Models/DTOs/ArticleDTO.cs
@@ -7,7 +7,7 @@ public class ArticleDTO
     public string title { get; set; } = null!;
     public ArticlePublisherDTO publisher { get; set; } = null!;
     public string author { get; set; } = null!;
-    public string published_utc { get; set; } = null!;
+    public DateTime published_utc { get; set; }
     public string amp_url { get; set; } = null!;
     public string image_url { get; set; } = null!;
     public string description { get; set; } = null!;
